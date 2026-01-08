5ef055de4175ffcd988e0e42a0cf72f7933669d9
Felix Miller <Felixmiller2005@outlook.de>
2026-01-08 22:48:05 +0100

    Fix connection closing in getDatabaseReady method

--- app/helpers.py
+++ app/helpers.py
@@ -8,6 +8,7 @@ class Helpers:
             with current_app.engine.connect().execution_options(timeout=2) as connection:
                 # Ein sicheres SQL-Statement ausführen
                 connection.execute(text("SELECT 1"))
+                connection.close()
                 return True, ""
         except Exception as e:
             return False, str(e)
