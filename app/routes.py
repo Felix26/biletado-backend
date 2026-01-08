1fb592ffbdea8a70df45cc78583091e30483fd8b
Felix Miller <Felixmiller2005@outlook.de>
2026-01-08 21:25:02 +0100

    Add API endpoint for reservation status with authors and version info

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,7 @@ def error_resp(code, msg, status=400):
         "errors": [{"code": code, "message": msg}],
         "trace": str(uuid.uuid4())
     }), status)
+
+@main_bp.route('/api/v3/reservations/status', methods=['GET'])
+def get_status():
+    return jsonify({"authors": ["Felix Miller", "Nik Wachsmann", "Ben Stahl"], "api_version": "3.0.0"})
