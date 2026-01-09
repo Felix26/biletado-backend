58f571ca956d626ad11f7e6617c5cc4ec05647fb
Felix Miller <FelixMiller2005@outlook.de>
2026-01-09 22:10:11 +0100

    Refactor app initialization to use configurable logging level and database URI

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from pythonjsonlogger import jsonlogger
 import logging
 from .models import db
+from .config import Config
 
 from sqlalchemy import create_engine
 
@@ -17,7 +18,7 @@ def create_app():
     formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(name)s %(message)s')
     logHandler.setFormatter(formatter)
     app.logger.addHandler(logHandler)
-    app.logger.setLevel(logging.INFO) #TODO: Configurable log level
+    app.logger.setLevel(Config.LOG_LEVEL)
 
     # Blueprints/Routes registrieren
     from .routes import main_bp
@@ -26,7 +27,7 @@ def create_app():
     
     # 1. Connection String definieren
     # Format: postgresql+psycopg://USER:PASSWORD@HOST:PORT/DB_NAME
-    db_url = "postgresql+psycopg://postgres:postgres@localhost:50000/reservations_v3" #TODO: Aus Konfiguration laden
+    db_url = Config.SQLALCHEMY_DATABASE_URI
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 
     db.init_app(app)
